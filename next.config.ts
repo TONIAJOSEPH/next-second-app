e88e102d9ebb439a46e8d2f2a3d5990bbe4a6097
Tonia Joseph <tonia.joseph@reflectionsinfos.com>
2025-08-01 12:02:57 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Export static HTML for GitHub Pages
-  basePath: "/next-second-app",
-  assetPrefix: "/next-second-app",
-  trailingSlash: true, // Recommended for routing to work correctly
 };
 
 export default nextConfig;
