d5d73a63bb363e1dedc58bd267c681ef5926be06
Tonia Joseph <tonia.joseph@reflectionsinfos.com>
2025-08-01 11:52:39 +0530

    add database connection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Export static HTML for GitHub Pages
+  basePath: "/next-second-app",
+  assetPrefix: "/next-second-app",
+  trailingSlash: true, // Recommended for routing to work correctly
 };
 
 export default nextConfig;
